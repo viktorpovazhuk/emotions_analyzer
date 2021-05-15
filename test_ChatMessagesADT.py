4790a6069dbff89a09d12d45fc9345962a4e0bb3
notnormasatall <taras.rodzin@ukr.net>
2021-05-15 22:25:28 +0300

    Created test modules

--- test_ChatMessagesADT.py
+++ test_ChatMessagesADT.py
@@ -1,15 +1,192 @@
 import unittest
 from unittest import TestCase
-from ChatMessagesADT import MessageADT
+from ChatMessagesADT import MessageADT, ChatMessagesADT, SentenceADT
 
 
 class TestMessageADT(TestCase):
-    def test_init(self):
+    """
+    Tests ChatMessagesADT, MessageADT, SentenceADT objects
+    """
+
+    def test_SADT_init(self):
+        """
+        Checks init of SentenceADT.
+        """
+        s1 = SentenceADT('My dog is a cat... ')
+        s1.emotion = 'sadness'
+
+        self.assertTrue(s1.emotion == 'sadness')
+
+    def test_SADT_str(self):
+        """
+        Checks str of SentenceADT.
+        """
+
+        s1 = SentenceADT('My dog is a cat... ')
+        self.assertTrue(str(s1) == 'My dog is a cat... ')
+
+    def test_MADT_init(self):
+        """
+        Checks init of MessageADT.
+        """
+
         text = 'I hope it works. It should work!'
         message = MessageADT(text)
         self.assertTrue(len(message) == 2)
-        self.assertTrue([sentence.text for sentence in message.sentences] == ['I hope it works', 'It should work'])
 
+    def test_CMADT_init(self):
+        """
+        Checks init of ChatMessagesADT.
+        """
+
+        m1 = MessageADT('I love my dog. My dog is good! ')
+        m2 = MessageADT('I hope it works. It should work! I hate it... ')
+
+        chm1 = ChatMessagesADT([m1, m2])
+        self.assertTrue(len(chm1.messages) == 2)
+
+    def test_CMADT_add_message(self):
+        """
+        Check add_message method from ChatMessagesADT.
+        """
+
+        m1 = MessageADT('I love my dog. My dog is good! ')
+        m2 = MessageADT('I hope it works. It should work! I hate it... ')
+
+        chm1 = ChatMessagesADT([m1])
+        self.assertTrue(
+            str(len(chm1.messages) == 1))
+
+        chm1.add_message(m2)
+
+        self.assertTrue(chm1.is_empty() == False)
+        self.assertTrue(
+            str(len(chm1.messages) == 2))
+
+    def test_CMADT_fill_emotion(self):
+        """
+        Check fill_emotion method from ChatMessagesADT.
+        """
+        s1 = SentenceADT('My dog is a cat... ')
+        s1.emotion = 'sadness'
+
+        m1 = MessageADT('I love my dog. My dog is good! ')
+        m1._add_sentence(s1)
+        m2 = MessageADT('I hope it works. It should work! ')
+        m2._add_sentence(SentenceADT('I hate it... ', 'anger'))
+
+        chm1 = ChatMessagesADT([m2])
+        chm1.add_message(m1)
+        self.assertTrue(chm1.emotions['sadness'] == 1)
+
+    def test_CMADT_delete_sentences(self):
+        """
+        Check delete_sentences method from ChatMessagesADT.
+        """
+        m1 = MessageADT('I hope it works. It should work! ')
+        m2 = MessageADT('I hope it works. It should work! ')
+        m2._add_sentence(SentenceADT('I hate it... ', 'anger'))
+
+        chm1 = ChatMessagesADT([m2])
+
+        chm1.delete_sentences('anger')
+        self.assertTrue(m2 == m1)
+
+    def test_CMADT_get_sentences(self):
+        """
+        Check get_sentences method from ChatMessagesADT.
+        """
+        result = ChatMessagesADT(
+            [MessageADT('I hope it works. ' + 'I hate it... ')])
+        m1 = MessageADT()
+        s1 = SentenceADT('I hope it works. ', 'anger')
+        s2 = SentenceADT('It should work! ', 'confident')
+        s3 = SentenceADT('I hate it... ', 'anger')
+
+        m1._add_sentence(s1)
+        m1._add_sentence(s2)
+        m1._add_sentence(s3)
+
+        chm1 = ChatMessagesADT([m1])
+
+        chm1.get_sentences('anger')
+        self.assertTrue(chm1 == result)
+
+    def test_CMADT_get_percentage(self):
+        """
+        Check get_percentage method from ChatMessagesADT.
+        """
+
+        m1 = MessageADT()
+        s1 = SentenceADT('I hope it works. ', 'anger')
+        s2 = SentenceADT('It should work! ', 'confident')
+        s3 = SentenceADT('I hate it... ', 'anger')
+
+        m1._add_sentence(s1)
+        m1._add_sentence(s2)
+        m1._add_sentence(s3)
+
+        chm1 = ChatMessagesADT([m1])
+
+        result = chm1.get_percentage('anger')
+        self.assertTrue(result == 66.67)
+
+    def test_CMADT_delete_sentences(self):
+        """
+        Check delete_sentences method from ChatMessagesADT.
+        """
+
+        m1 = MessageADT('I love my dog. My dog is good! ')
+        m2 = MessageADT('I hope it works. It should work! ')
+
+        chm1 = ChatMessagesADT([m1, m2])
+        chm1.delete_message(0)
+        self.assertTrue(len(chm1) == 1)
+
+    def test_CMADT_compare_ChatMessagesADT(self):
+        """
+        Compares two ChatMessagesADT.
+        """
+
+        m1 = MessageADT('I love my dog. My dog is good! ')
+        m2 = MessageADT('I love my cat. My dog is good! ')
+
+        chm1 = ChatMessagesADT([m1])
+        chm2 = ChatMessagesADT([m2])
+
+        self.assertTrue(chm1 != chm2)
+
+    def test_compare_MessageADT(self):
+        """
+        Compares two MessagesADT.
+        """
+
+        m1 = MessageADT('I love my dog. My dog is good! ')
+        m2 = MessageADT('I love my cat. My dog is good! ')
+
+        self.assertTrue(m1 != m2)
+
+    def test_compare_SentenceADT(self):
+        """
+        Compares two SentenceADT.
+        """
+
+        s1 = SentenceADT('I love my dog. ')
+        s2 = SentenceADT('I love my cat. ')
+
+        self.assertTrue(s1 != s2)
+
+    def test_CMADT_contains(self):
+        """
+        Check __contains__ method from ChatMessagesADT.
+        """
+
+        m1 = MessageADT('I love my dog. My dog is good! ')
+        m2 = MessageADT('I hope it works. It should work! ')
+
+        chm1 = ChatMessagesADT([m1, m2])
+        self.assertTrue('dog' in chm1)
+        self.assertTrue('dogg' not in chm1)
 
 
 if __name__ == "__main__":
--- test_text_tone.py
+++ test_text_tone.py
@@ -1,14 +1,23 @@
 import unittest
 from unittest import TestCase
-from text_tone import detect_tone
+from text_tone import detect_tone, translate_to_en
 
 
 class TestTextTone(TestCase):
+
     def test_detect_tone(self):
-        text = 'I love my dog'
-        emotion = detect_tone(text)
+
+        text1 = 'I love my dog'
+        emotion = detect_tone(text1)
         self.assertTrue(emotion == 'joy')
 
+        text2 = '''Лишилось тільки ще спакуватись... Се було одно з тих незчисленних "треба", \
+які мене так утомили і не давали спати. Дарма, чи те "треба" мале, чи велике, — \
+вагу те має, що кожен раз воно вимагає уваги, що не я їм, а воно мною уже керує. \
+Фактично стаєш невільником сього многоголового звіра. Хоч на час увільнитись \
+від нього, забути, спочити. Я утомився. '''
+        emotion = detect_tone(text2)
+        self.assertTrue(emotion == 'sadness')
 
 
 if __name__ == "__main__":
